18eb58c9daa6127456b5507e1c1a2f8208852e9b
Raul Ferrandez <raul.ferrandez@here.com>
2014-12-29 16:07:37 +0100

    foo

--- mainwindow.rb
+++ mainwindow.rb
@@ -10,7 +10,7 @@ class MainWindow < Qt::MainWindow
 	    createMenus()
 	end
 	
-	def about()
+	def about
 	   Qt::MessageBox.about(self, tr('Ecocity Porc'), tr( 'Autor: Raul Ferrandez Salvador' ) )
 	end
 	
@@ -18,16 +18,11 @@ class MainWindow < Qt::MainWindow
 	    @aboutAct = Qt::Action.new(tr("&About"), self)
 	    @aboutAct.statusTip = tr("Show the application's About box")
 	    connect(@aboutAct, SIGNAL('triggered()'), self, SLOT('about()'))
-	
-	    @aboutQtAct = Qt::Action.new(tr("About &Qt"), self)
-	    @aboutQtAct.statusTip = tr("Show the Qt library's About box")
-	    connect(@aboutQtAct, SIGNAL('triggered()'), $qApp, SLOT('aboutQt()'))
 	end
 	
 	def createMenus
 	    @helpMenu = menuBar().addMenu(tr("&Help"))
 	    @helpMenu.addAction(@aboutAct)
-	    @helpMenu.addAction(@aboutQtAct)
 	end
 
 end
