a52f6c8749827aa9897f3de8776fb0208b125549
Raul Ferrandez <raul.ferrandez@here.com>
2014-12-29 19:16:20 +0100

    foo

--- products_tab.rb
+++ products_tab.rb
@@ -5,33 +5,7 @@ class ProductsTab < Qt::Widget
   def initialize( products, parent = nil )
     super(parent)
 
-    data = "Designing a Component			Creating a GUI for your application\n"
-    data << "Creating a Dialog			How to create a dialog"
-    data << "Composing the Dialog		Putting widgets into the dialog example"
-    data << "Creating a Layout			Arranging widgets on a form"
-    data << "Signal and Slot Connections		Making widget communicate with each other"
-
-    data << "Using a Component in Your Application	Generating code from forms"
-    data << "The Direct Approach			Using a form without any adjustments"
-    data << "The Single Inheritance Approach	Subclassing a form's base class"
-    data << "The Multiple Inheritance Approach	Subclassing the form itself"
-    data << "Automatic Connections		Connecting widgets using a naming scheme"
-    data << "A Dialog Without Auto-Connect	How to connect widgets without a naming scheme"
-    data << "A Dialog With Auto-Connect	Using automatic connections"
-
-    data << "Form Editing Mode			How to edit a form in Qt Designer"
-    data << "Managing Forms			Loading and saving forms"
-    data << "Editing a Form			Basic editing techniques"
-    data << "The Property Editor			Changing widget properties"
-    data << "The Object Inspector		Examining the hierarchy of objects on a form"
-    data << "Layouts				Objects that arrange widgets on a form"
-    data << "Applying and Breaking Layouts	Managing widgets in layouts"
-    data << "Horizontal and Vertical Layouts	Standard row and column layouts"
-    data << "The Grid Layout			Arranging widgets in a matrix"
-    data << "Previewing Forms			Checking that the design works"
-
-
-    products_model = ProductsTreeModel.new( data )
+    products_model = ProductsTreeModel.new( products )
 
     view = Qt::TreeView.new
     view.model = products_model
--- products_tree_model.rb
+++ products_tree_model.rb
@@ -2,12 +2,12 @@ require 'product_tree_item'
 
 class ProductsTreeModel < Qt::AbstractItemModel
 	
-	def initialize( data, parent = nil )
+	def initialize( products, parent = nil )
     super( parent )
     root_data = []
     root_data << 'Nom' << 'Descripcio'
     @root_item = ProductTreeItem.new( root_data )
-    setupModelData( data.to_s.split("\n"), @root_item )
+    setupModelData( products, @root_item )
 	end
 	
 	def column_count(parent)
@@ -89,53 +89,18 @@ class ProductsTreeModel < Qt::AbstractItemModel
 
   alias :rowCount :row_count
 	
-	def setup_model_data(lines, parent)
+	def setup_model_data( products, parent)
     parents = []
     indentations = []
     parents << parent
     indentations << 0
 
-    number = 0
-
-    while number < lines.length
-      position = 0
-      while position < lines[number].length
-        if lines[number][position, 1] != " "
-          break
-        end
-        position += 1
-      end
-
-      line_data = lines[number][position, lines[number].length].strip
-
-      if !line_data.empty?
-      # Read the column data from the rest of the line.
-      column_strings = line_data.split("\t").delete_if {|item| item == ""}
-      column_data = []
-      for column in 0...column_strings.length
-        column_data << column_strings[column]
-      end
-
-      if position > indentations.last
-        # The last child of the current parent is now the parent.new
-        # unless the current parent has no children.
-        if parents.last.childCount > 0
-          parents << parents.last.child(parents.last.childCount - 1)
-          indentations << position
-        end
-      else
-        while position < indentations.last && parents.length > 0
-          parents.pop
-          indentations.pop
-        end
-      end
-
-      # Append a item.new to the current parent's list of children.
+    products.each_with_index do | product, index |
+      column_string = [ product.name, product.price_tienda ]
+      column_data = column_string
       parents.last.appendChild(ProductTreeItem.new(column_data, parents.last))
     end
-	
-	  number += 1
-	  end
+
   end
 
   alias :setupModelData :setup_model_data
