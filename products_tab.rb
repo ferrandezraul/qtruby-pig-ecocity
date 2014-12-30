9f25ba739b19ea0ee6a41addde188a943b17afe2
Raul Ferrandez <raul.ferrandez@here.com>
2014-12-30 16:21:23 +0100

    foo

--- products_tab.rb
+++ products_tab.rb
@@ -1,4 +1,3 @@
-require 'products_tree_model'
 require 'logger'
 
 class ProductsTab < Qt::Widget
@@ -7,9 +6,7 @@ class ProductsTab < Qt::Widget
     super(parent)
     @log = Logger.new('development.log', 'monthly')
 
-    #products_model = ProductsTreeModel.new( products )
-
-    products_model = Qt::StandardItemModel.new( products.length, 6, self )
+    products_model = Qt::StandardItemModel.new( products.length, 7, self )
     products_model.setHeaderData( 0, Qt::Horizontal, Qt::Variant.new( tr( 'Name' ) ) )
     products_model.setHeaderData( 1, Qt::Horizontal, Qt::Variant.new( tr( 'Preu Tenda' ) ) )
     products_model.setHeaderData( 2, Qt::Horizontal, Qt::Variant.new( tr( 'Preu Coope' ) ) )
@@ -20,48 +17,36 @@ class ProductsTab < Qt::Widget
 
 
     products.each_with_index do | product, row |
-      # insertRows ( int row, int count, const QModelIndex & parent = QModelIndex() )
-      products_model.insertRows( row, 1 )
-
-      # setData ( const QModelIndex & index, const QVariant & value, int role = Qt::EditRole )
-      products_model.setData( products_model.index( row, 0 ), Qt::Variant.new( product.name ) )
-
-      products_model.setData( products_model.index( row, 1 ), Qt::Variant.new( "#{product.price_tienda} EUR" ) )
-      products_model.setData( products_model.index( row, 1 ), qVariantFromValue( Qt::AlignRight ), Qt::TextAlignmentRole )
-
-      products_model.setData( products_model.index( row, 2 ), Qt::Variant.new( "#{product.price_coope} EUR" ) )
-      products_model.setData( products_model.index( row, 2 ), qVariantFromValue( Qt::AlignRight ), Qt::TextAlignmentRole )
-
-      products_model.setData( products_model.index( row, 3 ), Qt::Variant.new( "#{product.price_pvp} EUR") )
-      products_model.setData( products_model.index( row, 3 ), qVariantFromValue( Qt::AlignRight ), Qt::TextAlignmentRole )
-
-      products_model.setData( products_model.index( row, 4 ), Qt::Variant.new( product.price_type ) )
-      products_model.setData( products_model.index( row, 4 ), qVariantFromValue( Qt::AlignRight ), Qt::TextAlignmentRole )
-
-      products_model.setData( products_model.index( row, 5 ), Qt::Variant.new( "#{product.iva} %" ) )
-      products_model.setData( products_model.index( row, 5 ), qVariantFromValue( Qt::AlignRight ), Qt::TextAlignmentRole )
-
-      products_model.setData( products_model.index( row, 6 ), Qt::Variant.new( "#{product.weight_per_unit} Kg/Un" ) )
-      products_model.setData( products_model.index( row, 6 ), qVariantFromValue( Qt::AlignRight ), Qt::TextAlignmentRole )
+      item_name = Qt::StandardItem.new( product.name )
+      item_price_tienda = Qt::StandardItem.new( "#{product.price_tienda} EUR" )
+      item_price_coope = Qt::StandardItem.new( "#{product.price_coope} EUR" )
+      item_price_pvp = Qt::StandardItem.new( "#{product.price_pvp} EUR" )
+      item_price_type = Qt::StandardItem.new( product.price_type )
+      item_price_iva = Qt::StandardItem.new( "#{product.iva} %" )
+      item_weight_per_unit = Qt::StandardItem.new( "#{product.weight_per_unit} Kg/Un" )
 
       if product.has_options?
-        parent_index = products_model.index( row, 0 )
-        @log.debug( "Parent index contains #{parent_index.data.toString}")
-        @log.debug( "Parent index is valid #{parent_index.isValid}")
         product.options.each_with_index do | option, option_row |
-          inserted = products_model.insertRows( option_row, 1, parent_index )
-          @log.debug( "Child row inserted #{inserted} for option name #{option.name}" )
-          child_model_index = products_model.index( row, 0 ).child( option_row, 0 )
-
-          @log.debug( "New model index #{child_model_index.isValid}" )
-          setted = products_model.setData( child_model_index, Qt::Variant.new( option.name ) )
-          @log.debug( "Data set #{setted}")
-
-          index_inserted = products_model.index( row, 0 ).child( option_row, 0 )
-          @log.debug( "Data inserted #{index_inserted.data.toString}" )
+          item_option_name = Qt::StandardItem.new( option.name )
+          item_name.setChild( option_row, 0, item_option_name )
         end
       end
 
+      # setItem ( int row, QStandardItem * item )
+      products_model.setItem( row, 0, item_name )
+      products_model.setItem( row, 1, item_price_tienda )
+      products_model.setItem( row, 2, item_price_coope )
+      products_model.setItem( row, 3, item_price_pvp )
+      products_model.setItem( row, 4, item_price_type )
+      products_model.setItem( row, 5, item_price_iva )
+      products_model.setItem( row, 6, item_weight_per_unit )
+
+      products_model.setData( products_model.index( row, 1 ), qVariantFromValue( Qt::AlignRight ), Qt::TextAlignmentRole )
+      products_model.setData( products_model.index( row, 2 ), qVariantFromValue( Qt::AlignRight ), Qt::TextAlignmentRole )
+      products_model.setData( products_model.index( row, 3 ), qVariantFromValue( Qt::AlignRight ), Qt::TextAlignmentRole )
+      products_model.setData( products_model.index( row, 4 ), qVariantFromValue( Qt::AlignRight ), Qt::TextAlignmentRole )
+      products_model.setData( products_model.index( row, 5 ), qVariantFromValue( Qt::AlignRight ), Qt::TextAlignmentRole )
+      products_model.setData( products_model.index( row, 6 ), qVariantFromValue( Qt::AlignRight ), Qt::TextAlignmentRole )
     end
 
     view = Qt::TreeView.new
