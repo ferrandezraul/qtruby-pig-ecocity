837515c6e9adf178849acf24c88694929d43a224
Raul Ferrandez <raul.ferrandez@here.com>
2014-12-30 13:29:48 +0100

    foo

--- products_tab.rb
+++ products_tab.rb
@@ -21,23 +21,26 @@ class ProductsTab < Qt::Widget
       # insertRows ( int row, int count, const QModelIndex & parent = QModelIndex() )
       products_model.insertRows( row, 1, Qt::ModelIndex.new() )
 
-      item_name = Qt::StandardItem.new(  product.name )
-      item_name.setData( qVariantFromValue( Qt::AlignRight ), Qt::TextAlignmentRole )
-
-      # setItem ( int row, QStandardItem * item )
-      products_model.setItem( row, 0, item_name )
-
       # setData ( const QModelIndex & index, const QVariant & value, int role = Qt::EditRole )
-      #products_model.setData( products_model.index( row, 0, Qt::ModelIndex.new), Qt::Variant.new( product.name ) )
+      products_model.setData( products_model.index( row, 0, Qt::ModelIndex.new), Qt::Variant.new( product.name ) )
 
       products_model.setData( products_model.index( row, 1, Qt::ModelIndex.new), Qt::Variant.new( "#{product.price_tienda} EUR" ) )
-      #products_model.item( row, 1).setData( Qt::AlignRight, Qt::TextAlignmentRole )
+      products_model.setData( products_model.index( row, 1, Qt::ModelIndex.new), qVariantFromValue( Qt::AlignRight ), Qt::TextAlignmentRole )
 
-      products_model.setData( products_model.index( row, 2, Qt::ModelIndex.new), Qt::Variant.new( product.price_coope ) )
-      products_model.setData( products_model.index( row, 3, Qt::ModelIndex.new), Qt::Variant.new( product.price_pvp ) )
-      products_model.setData( products_model.index( row, 4, Qt::ModelIndex.new), Qt::Variant.new( product.price_type ) )
-      products_model.setData( products_model.index( row, 5, Qt::ModelIndex.new), Qt::Variant.new( product.iva ) )
-      products_model.setData( products_model.index( row, 6, Qt::ModelIndex.new), Qt::Variant.new( product.weight_per_unit ) )
+      products_model.setData( products_model.index( row, 2, Qt::ModelIndex.new), Qt::Variant.new( "#{product.price_coope} EUR" ) )
+      products_model.setData( products_model.index( row, 2, Qt::ModelIndex.new), qVariantFromValue( Qt::AlignRight ), Qt::TextAlignmentRole )
+
+      products_model.setData( products_model.index( row, 3, Qt::ModelIndex.new), Qt::Variant.new( "#{product.price_pvp} EUR") )
+      products_model.setData( products_model.index( row, 3, Qt::ModelIndex.new), qVariantFromValue( Qt::AlignRight ), Qt::TextAlignmentRole )
+
+      products_model.setData( products_model.index( row, 4, Qt::ModelIndex.new), Qt::Variant.new( "#{product.price_type} EUR" ) )
+      products_model.setData( products_model.index( row, 4, Qt::ModelIndex.new), qVariantFromValue( Qt::AlignRight ), Qt::TextAlignmentRole )
+
+      products_model.setData( products_model.index( row, 5, Qt::ModelIndex.new), Qt::Variant.new( "#{product.iva} %" ) )
+      products_model.setData( products_model.index( row, 5, Qt::ModelIndex.new), qVariantFromValue( Qt::AlignRight ), Qt::TextAlignmentRole )
+
+      products_model.setData( products_model.index( row, 6, Qt::ModelIndex.new), Qt::Variant.new( "#{product.weight_per_unit} Kg/Un" ) )
+      products_model.setData( products_model.index( row, 6, Qt::ModelIndex.new), qVariantFromValue( Qt::AlignRight ), Qt::TextAlignmentRole )
     end
 
     view = Qt::TreeView.new
