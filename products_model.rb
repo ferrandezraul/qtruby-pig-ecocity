51b049af82a3159014cb1f3052f00c0c7ccf4b95
Raul Ferrandez <raul.ferrandez@here.com>
2014-12-30 22:00:09 +0100

    foo

--- products_model.rb
+++ products_model.rb
@@ -1,7 +1,16 @@
 class ProductsModel
 
+  COLUMN_NAME            = 0
+  COLUMN_PRICE_TIENDA    = 1
+  COLUMN_PRICE_COOPE     = 2
+  COLUMN_PRICE_PVP       = 3
+  COLUMN_PRICE_TYPE      = 4
+  COLUMN_IVA             = 5
+  COLUMN_WEIGHT_PER_UNIT = 6
+  NUMBER_OF_COLUMNS      = 7
+
   def self.get_model( products, parent )
-    products_model = Qt::StandardItemModel.new( products.length, 7, parent )
+    products_model = Qt::StandardItemModel.new( products.length, NUMBER_OF_COLUMNS, parent )
     products_model.setHeaderData( 0, Qt::Horizontal, Qt::Variant.new( 'Nom' ) )
     products_model.setHeaderData( 1, Qt::Horizontal, Qt::Variant.new( 'Preu Tenda' ) )
     products_model.setHeaderData( 2, Qt::Horizontal, Qt::Variant.new( 'Preu Coope' ) )
@@ -22,18 +31,18 @@ class ProductsModel
       if product.has_options?
         product.options.each_with_index do | option, option_row |
           item_option_name = Qt::StandardItem.new( option.name )
-          item_name.setChild( option_row, 0, item_option_name )
+          item_name.setChild( option_row, COLUMN_NAME, item_option_name )
         end
       end
 
       # setItem ( int row, QStandardItem * item )
-      products_model.setItem( row, 0, item_name )
-      products_model.setItem( row, 1, item_price_tienda )
-      products_model.setItem( row, 2, item_price_coope )
-      products_model.setItem( row, 3, item_price_pvp )
-      products_model.setItem( row, 4, item_price_type )
-      products_model.setItem( row, 5, item_price_iva )
-      products_model.setItem( row, 6, item_weight_per_unit )
+      products_model.setItem( row, COLUMN_NAME, item_name )
+      products_model.setItem( row, COLUMN_PRICE_TIENDA, item_price_tienda )
+      products_model.setItem( row, COLUMN_PRICE_COOPE, item_price_coope )
+      products_model.setItem( row, COLUMN_PRICE_PVP, item_price_pvp )
+      products_model.setItem( row, COLUMN_PRICE_TYPE, item_price_type )
+      products_model.setItem( row, COLUMN_IVA, item_price_iva )
+      products_model.setItem( row, COLUMN_WEIGHT_PER_UNIT, item_weight_per_unit )
 
       products_model.setData( products_model.index( row, 1 ), Qt::Variant.fromValue( Qt::AlignRight ), Qt::TextAlignmentRole )
       products_model.setData( products_model.index( row, 2 ), Qt::Variant.fromValue( Qt::AlignRight ), Qt::TextAlignmentRole )
