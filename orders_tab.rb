1b5379d3b515ea89c7340d3f8c2a328e56430d21
Raul Ferrandez <raul.ferrandez@here.com>
2014-12-31 11:22:07 +0100

    foo

--- orders_tab.rb
+++ orders_tab.rb
@@ -1,20 +1,30 @@
 class OrdersTab < Qt::Widget
 
+  slots 'new_order()'
+
   def initialize( orders_model, parent = nil )
     super(parent)
 
+    button = Qt::PushButton.new( tr( "Nova comanda" ) )
     view = Qt::TreeView.new
     view.model = orders_model
     view.windowTitle = 'Orders Tree Model'
     view.show
 
     self.layout = Qt::VBoxLayout.new do |m|
+      m.addWidget( button )
       m.addWidget( view )
 
       # Add spacer
       #m.addStretch( 1 )
     end
 
+    connect( button, SIGNAL( 'clicked()' ), self, SLOT( 'new_order()' ) )
+
+  end
+
+  def new_order
+    Qt::MessageBox::information( self, tr( 'New order dialog!' ), 'New order')
   end
 
 end
