89ffac511054c726c3effd77427ec109b4fe663f
Raul Ferrandez <raul.ferrandez@here.com>
2015-01-05 17:07:46 +0100

    foo

---
+++ new_order_dialog.rb
@@ -0,0 +1,46 @@
+require 'date_dialog'
+require 'customer_dialog'
+require 'line_items_dialog'
+
+class NewOrderDialog < Qt::Widget
+  attr_reader :customer
+  attr_reader :date
+  attr_reader :line_items
+
+  def initialize( products, customers, parent = nil )
+    super( parent )
+
+    get_date
+    get_customer( customers )
+    get_line_items(@customer, products)
+  end
+
+  # Returns a Qt::Date object
+  def get_date
+    date_dialog = DateDialog.new(self)
+    if date_dialog.exec == 1  # if user accepted the dialog
+      @date = date_dialog.get_date
+    else
+      @date = Qt::Date.new
+    end
+  end
+
+  def get_customer( customers )
+    customer_dialog = CustomerDialog.new( customers, self)
+    if customer_dialog.exec == 1  # if user accepted the dialog
+      @customer = customer_dialog.get_customer
+    else
+      @customer = String.new
+    end
+  end
+
+  def get_line_items( customer, products )
+    line_items_dialog = LineItemsDialog.new( products, customer, self )
+    if line_items_dialog.exec == 1
+      @line_items = line_items_dialog.get_line_items
+    else
+      @line_items = Array.new
+    end
+  end
+
+end
--- orders_tab.rb
+++ orders_tab.rb
@@ -1,11 +1,10 @@
-require 'date_dialog'
-require 'customer_dialog'
-require 'line_items_dialog'
 require 'orders_model'
 
 require 'order'
 require 'ap'
 require 'date'
+
+require 'new_order_dialog'
 
 class OrdersTab < Qt::Widget
 
@@ -41,42 +40,15 @@ class OrdersTab < Qt::Widget
   end
 
   def new_order
-    qdate = get_date
+    order_dialog = NewOrderDialog.new( @products, @customers, self )
+
+    qdate = order_dialog.date
     date = Date.new( qdate.year, qdate.month, qdate.day )
-    customer = get_customer
-    line_items = get_line_items( customer )
+    customer = order_dialog.customer
+    line_items = order_dialog.line_items
 
     order = Order.new( customer, date, line_items )
     OrdersModel.add_order_to_model( order, @orders_model )
-  end
-
-
-  # Returns a Qt::Date object
-  def get_date
-    date_dialog = DateDialog.new(self)
-    if date_dialog.exec == 1  # if user accepted the dialog
-      date_dialog.get_date
-    else
-      Qt::Date.new
-    end
-  end
-
-  def get_customer
-    customer_dialog = CustomerDialog.new( @customers, self)
-    if customer_dialog.exec == 1  # if user accepted the dialog
-      customer_dialog.get_customer
-    else
-      String.new
-    end
-  end
-
-  def get_line_items( customer )
-    line_items_dialog = LineItemsDialog.new( @products, customer, self )
-    if line_items_dialog.exec == 1
-      line_items_dialog.get_line_items
-    else
-      Array.new
-    end
   end
 
 end
